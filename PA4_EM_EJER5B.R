c5993fb95a48a2f1ceff3bf000cd6cf5e06b94d9
Niki Kiedanski <nicole@192.168.1.5>
2022-10-06 19:49:41 -0300

    dist binary was solved and code writen until eigen(Q)

--- PA4_EM_EJER5B.R
+++ PA4_EM_EJER5B.R
@@ -11,8 +11,8 @@ data
 #con coeficiente de similaridad de Sokal-Michener (cant. variables con resp 1 
 #en ambos individuos + cant de variables con resp 0 en ambos individuos)/ cant total variables
 
-install.packages("clusterSim")
+# install.packages("clusterSim")
 library(clusterSim)
 
 #Matriz de distancia al cuadrado:
-D2= as.matrix(2* dist.binary(data, method=2, diag=T,upper=T) ^2)
+D2 <- as.matrix(2* dist.binary(data, method=2, diag=T,upper=T) ^2)
