153b3952cfba4c2c2589d7faff5d20155af64364
Niki Kiedanski <nicole@192.168.1.4>
2022-11-04 15:45:09 -0300

    finalize exer 3

--- PA5_CLUST_EJER3.R
+++ PA5_CLUST_EJER3.R
@@ -1,7 +1,3 @@
-library(factoextra)
-library(NbClust)
-
-
 # a) se aplica k-means fijando los centroides iniciales y se calcula la inercia intra-cluster
 
 # caso 1
@@ -10,7 +6,8 @@ x = c(1, 2, 9, 12, 20)
 c1 = 1
 c2 = 20
 # se aplica k-means
-cl = kmeans(x, c(c1, c2), iter.max=2)
+cl = kmeans(x, c(c1, c2))
+cat("La inercia intra Clusters cuando k = 2 es: ", cl$tot.withinss)
 plot(x, col = cl$cluster, main = "K-Means Clustering Results with K = 2",
      xlab = "", ylab = "")
 points(cl$centers, col = 1:2, pch = 8, cex = 2)
@@ -19,8 +16,8 @@ points(cl$centers, col = 1:2, pch = 8, cex = 2)
 c1 = 1
 c2 = 12
 c3 = 20
-
-cl = kmeans(x, c(c1, c2, c3), iter.max=2)
+cl = kmeans(x, c(c1, c2, c3))
+cat("La inercia intra Clusters cuando k = 3 es: ", cl$tot.withinss)
 plot(x, col = cl$cluster, main = "K-Means Clustering Results with K = 3",
      xlab = "", ylab = "")
 points(cl$centers, col = 1:3, pch = 8, cex = 2)
@@ -30,11 +27,18 @@ c1 = 1
 c2 = 9
 c3 = 12
 c4 = 20
-
-cl = kmeans(x, c(c1, c2, c3, c4), iter.max=2)
+cl = kmeans(x, c(c1, c2, c3, c4))
+cat("La inercia intra Clusters cuando k = 4 es: ", cl$tot.withinss)
 plot(x, col = cl$cluster, main = "K-Means Clustering Results with K = 4",
      xlab = "", ylab = "")
 points(cl$centers, col = 1:4, pch = 8)
 
-fviz_nbclust(x, kmeans, method = "silhouette")
+#La mejor opcion seria el caso 3 que tiene 4 clusters porque la inercia 
+# intra-cluster es menor, osea que los grupos son mas homogeneos
+
+# b) Metodo jerarquico ascendente y Dendograma
+
+hc = hclust(dist(x), method = "single")
+plot(hc, main = "Single Linkage",
+       xlab = "", sub = "", cex = .9)
 
